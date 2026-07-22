0d5d632e3a035764adfb4f7ac0f749ca22ac3105
yunjichu <yunjisu4645@gmail.com>
2026-07-22 16:12:25 +0900

    add SG(hap-soc-server-sg)

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -133,6 +133,14 @@ resource "aws_vpc_security_group_ingress_rule" "soc_server_self_neo4j_bolt" {
   to_port                      = 7687
 }
 
+resource "aws_vpc_security_group_ingress_rule" "soc_server_self_fastapi" {
+  security_group_id            = aws_security_group.soc_server.id
+  referenced_security_group_id = aws_security_group.soc_server.id
+  ip_protocol                  = "tcp"
+  from_port                    = 8000
+  to_port                      = 8000
+}
+
 resource "aws_vpc_security_group_ingress_rule" "soc_db_from_server" {
   security_group_id            = aws_security_group.soc_db.id
   referenced_security_group_id = aws_security_group.soc_server.id
