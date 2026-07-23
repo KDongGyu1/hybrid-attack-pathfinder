5c6edabb4bf06507f3869cd3ce2767f5ace09745
yunjichu <yunjisu4645@gmail.com>
2026-07-23 14:12:02 +0900

    add(sg): allow collector -> soc_server FastAPI(8000) inbound
    collector가 graph API(soc_server:8000)를 호출할 수 있도록 hap-soc-server-sg에 인바운드 규칙 추가.

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -141,6 +141,14 @@ resource "aws_vpc_security_group_ingress_rule" "soc_server_self_fastapi" {
   to_port                      = 8000
 }
 
+resource "aws_vpc_security_group_ingress_rule" "soc_server_from_collector_fastapi" {
+  security_group_id            = aws_security_group.soc_server.id
+  referenced_security_group_id = aws_security_group.soc_collector.id
+  ip_protocol                  = "tcp"
+  from_port                    = 8000
+  to_port                      = 8000
+}
+
 resource "aws_vpc_security_group_ingress_rule" "soc_db_from_server" {
   security_group_id            = aws_security_group.soc_db.id
   referenced_security_group_id = aws_security_group.soc_server.id
